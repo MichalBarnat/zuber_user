0677693e859455e6079fe878050d0450fdef701d
Krystian Bogacz <pefekbiznes@gmail.com>
2023-11-27 22:29:16 +0100

    Text add to RideRequest enums

--- zuber/src/main/java/com/bbc/zuber/model/rideRequest/enums/Size.java
+++ zuber/src/main/java/com/bbc/zuber/model/rideRequest/enums/Size.java
@@ -4,3 +4,12 @@ public enum Size {
     SINGLE,
     GROUP
 }
+ /*
+ Tutaj warto dodać value, które będzie determinować ilość miejsc dostęponych w taxi.
+ Zakładając, że auta osobowe to mogą być 4 miejsca jak np. dla S7 które może być taxi premium
+ to wartość dla GROUP ustawimy na 8 żeby wymusić dostępność busa.
+ Dodatkowo trzeba dla nich ustawić wartość żeby odpowiednio przejazd grupowy był droższy od single.
+ Proponuje wartości: numberOfSeats oraz value czyli kolejno to będzie:
+ SINGLE (numberOfSeats 1, value 1)
+ GROUP (numberOfSeats 8, value 3)
+  */
--- zuber/src/main/java/com/bbc/zuber/model/rideRequest/enums/Type.java
+++ zuber/src/main/java/com/bbc/zuber/model/rideRequest/enums/Type.java
@@ -4,4 +4,12 @@ public enum Type {
     STANDARD,
     ECO,
     PREMIUM
+
 }
+ /*
+ Tutaj warto dodać value, które będzie określać stawkę w zależności od typu przejazdu.
+ Elektryki jebać dlatego ECO będzie najdroższe.
+ STANDARD (value 1)
+ ECO (value 10)
+ PREMIUM (value 5)
+  */
