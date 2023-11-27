c2cb4444aa564f3ee1d84c3797ae1ea23963867d
Krystian Bogacz <pefekbiznes@gmail.com>
2023-11-27 22:20:31 +0100

    RideRequest command add(CreateRideRequestCommand & UpdateRideRequestCommand)

---
+++ zuber/src/main/java/com/bbc/zuber/model/rideRequest/command/CreateRideRequestCommand.java
@@ -0,0 +1,19 @@
+package com.bbc.zuber.model.rideRequest.command;
+
+import com.bbc.zuber.model.rideRequest.enums.Size;
+import com.bbc.zuber.model.rideRequest.enums.Type;
+import lombok.*;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class CreateRideRequestCommand {
+    private String pickUpLocation;
+    private String dropOffLocation;
+    private Type type;
+    private Size size;
+    private String date;
+}
