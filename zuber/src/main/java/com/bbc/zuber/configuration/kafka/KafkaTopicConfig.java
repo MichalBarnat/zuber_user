b8a2552692fa7e8e4524d849c458d53bb61fd4a3
Krystian Bogacz <pefekbiznes@gmail.com>
2023-11-28 09:01:18 +0100

    KafkaTopicConfig update

--- zuber/src/main/java/com/bbc/zuber/configuration/kafka/KafkaTopicConfig.java
+++ zuber/src/main/java/com/bbc/zuber/configuration/kafka/KafkaTopicConfig.java
@@ -1,4 +1,28 @@
 package com.bbc.zuber.configuration.kafka;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.TopicBuilder;
+
 public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic userRegistration() {
+        return TopicBuilder.name("user-registration")
+                .build();
+    }
+
+    @Bean
+    public NewTopic userDelete() {
+        return TopicBuilder.name("user-deleted")
+                .build();
+    }
+
+    @Bean
+    public NewTopic userEdit() {
+        return TopicBuilder.name("user-edited")
+                .build();
+    }
+
+
 }
