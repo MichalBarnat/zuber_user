6bfe7a1db1a99abadb035c36a00bb924cada23f8
Krystian Bogacz <pefekbiznes@gmail.com>
2023-12-06 19:13:24 +0100

    Logger added to KafkaListeners

--- zuber/src/main/java/com/bbc/zuber/kafka/KafkaListeners.java
+++ zuber/src/main/java/com/bbc/zuber/kafka/KafkaListeners.java
@@ -2,7 +2,11 @@ package com.bbc.zuber.kafka;
 
 import com.bbc.zuber.model.rideinfo.RideInfo;
 import com.bbc.zuber.service.RideInfoService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -10,10 +14,14 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class KafkaListeners {
     private final RideInfoService rideInfoService;
+    private final ObjectMapper objectMapper;
+
+    private final Logger logger = LoggerFactory.getLogger(KafkaListeners.class);
 
     @KafkaListener(topics = "ride-info")
-    void rideInfoListener(RideInfo rideInfo) {
-        RideInfo savedRideInfo = rideInfoService.save(rideInfo);
+    void rideInfoListener(String rideInfoString) throws JsonProcessingException {
+        RideInfo savedRideInfo = objectMapper.readValue(rideInfoString, RideInfo.class);
+        rideInfoService.save(savedRideInfo);
         System.out.println("Your info about ride: ");
         System.out.println(savedRideInfo);
         System.out.println("Have a good trip!");
