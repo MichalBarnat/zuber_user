08ac3843590e50227d8c48715ef05699d8fe1424
Michał Barnat <michalbarnat@gmail.com>
2023-12-12 12:21:24 +0100

    added SEQUENCE generator to Car Entity and update it correctly in liquibase

--- zuber/src/main/java/com/bbc/zuber/model/user/User.java
+++ zuber/src/main/java/com/bbc/zuber/model/user/User.java
@@ -18,7 +18,8 @@ import java.util.UUID;
 @NoArgsConstructor
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "users_seq")
+    @SequenceGenerator(name = "users_seq", sequenceName = "users_seq", allocationSize = 1)
     private Long id;
     private UUID uuid;
     private String name;
